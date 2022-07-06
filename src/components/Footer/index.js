5ac94160ac85f9bf1632e30dd6f4730ea25f8f57
Chantz-J <johnson.chantz@gmail.com>
2022-07-06 03:03:17 -0400

    add hero link to footer, and remove them from hero

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,76 +1,35 @@
 import * as React from "react";
-import { Container, Flex, Box, Text, Subhead } from "../ui"
+import { Container, Flex, Box, Text, Subhead, Button } from "../ui";
+import { Linkedin, GitHub, Codepen, } from "react-feather";
+import { footerLinkSpacing, footerOutro } from "./index.css";
+import { footerBorder } from "./index.css";
 
-  import { copyRightSpacing, footerLinkSpacing, footerOutro } from "./index.css";
-
-  import { footerBorder } from "./index.css";
-
-  export default function Footer () {
+export default function Footer () {
     return (
         <Box as="footer" className={footerBorder}>
             <Container>
-                
-                {/* <Flex variant="responsive" className={footerLinkSpacing}>
-
-                <Flex variant="spaceEvenly">
-                    <Flex gap={4} variant="column">
+                <Flex variant="responsive" className={footerLinkSpacing}>
+                    <Flex className={footerOutro} variant="columnStart">
+                        <Subhead as="h2">
+                            Simple digital experiences <br></br> with a human-centered approach.
+                        </Subhead>
                         <Text>
-                        Home
-                        </Text>
-                        <Text>
-                        About
-                        </Text>
-                        <Text>
-                        Blog
-                        </Text>
-                        <Text>
-                        Contact
+                            © 2022 Chantz Johnson. All rights reserved
                         </Text>
                     </Flex>
-                </Flex>
-
-                <Flex variant="spaceEvenly">
-                    <Flex gap={4} variant="column">
-                        <Text>
-                        LinkedIn
-                        </Text>
-                        <Text>
-                        Codepen
-                        </Text>
-                        <Text>
-                        Github
-                        </Text>
-                        <Text>
-                        Buy Me a Coffee
-                        </Text>
+                    <Flex gap={4} variant="responsive">
+                        <Button variant="link" to={'https://www.linkedin.com/in/chantzjohnson/'}>
+                            <Linkedin />
+                        </Button>
+                        <Button variant="link" to={'https://github.com/Chantz-J'} >
+                            <GitHub />
+                        </Button>
+                        <Button variant="link" to={'https://codepen.io/Moon_River'}>
+                            <Codepen />
+                        </Button>
                     </Flex>
                 </Flex>
-                <Flex variant="spaceEvenly">
-                    <Flex gap={4} variant="column">
-                        <Text>
-                        Privacy Policy
-                        </Text>
-                        <Text>
-                        Sitemap
-                        </Text>
-                    </Flex>
-                </Flex>
-
-                </Flex> */}
-
-                <Flex className={footerOutro}>
-                    <Subhead as="h2">
-                        Simple digital experiences <br></br> with a human-centered approach.
-                    </Subhead>
-                </Flex>
-
-                <Flex className={copyRightSpacing}>
-                    <Text>
-                        © 2022 Chantz Johnson. All rights reserved
-                    </Text>
-                </Flex>
-
             </Container>
         </Box>
-    )
-  }
+    );
+  };
